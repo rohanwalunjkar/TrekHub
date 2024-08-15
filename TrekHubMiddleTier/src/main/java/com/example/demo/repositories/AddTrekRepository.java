25c02d0590ce4534a3de3f4ca1c6ceea0fa6b83c
rohanwalunjkar <walunjkarrohan@gmail.com>
2024-08-15 14:12:15 +0530

    TrekHub Backendby Java

---
+++ TrekHubMiddleTier/src/main/java/com/example/demo/repositories/AddTrekRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repositories;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.AddTrek;
+
+@Transactional
+@Repository
+public interface AddTrekRepository extends JpaRepository<AddTrek, Integer> 
+{
+	
+
+
+}
